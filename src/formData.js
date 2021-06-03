64630c9b9112de72e65e80c0d8089d5476df10e0
Vadim.Frolov <vadimf74@ya.ru>
2021-06-03 21:24:46 +0300

    fixes

---
+++ src/formData.js
@@ -0,0 +1,12 @@
+const profile = {
+    profile: [
+        { category: 'Почта', content: 'pochta@yandex.ru' },
+        { category: 'Логин', content: 'ivanovivan' },
+        { category: 'Имя', content: 'Иван' },
+        { category: 'Фамилия', content: 'Иванов' },
+        { category: 'Имя в чате', content: 'Ваня' },
+        { category: 'Телефон', content: '8 942 54 33 43' },
+    ],
+}
+
+module.exports = profile
