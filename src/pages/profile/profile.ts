76eea3d90006f672c992a16bd461501b50fabd80
Vadim.Frolov <vadimf74@ya.ru>
2021-06-20 17:34:10 +0300

    fix scss files

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -3,6 +3,7 @@ import compiledTemplate from './profile.hbs'
 import data from './profile-data'
 import '../../helpers/handlebarsHelpers'
 import '../../layouts/base/base'
+import './profile.scss'
 
 export type Props = {
     profile: { category: string; content: string }[]
